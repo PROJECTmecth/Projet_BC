90b41cfa27da06f0c7499c25e108362a98e081b0
Mechack007 <roscabnagoulou@icloud.com>
2026-03-09 02:26:29 +0100

    Tache :Mise en commentaire des route middlware ,par manque de controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Routes protégées Admin (CheckIsAdmin middleware)
+/* Route::middleware(['auth:sanctum', 'isAdmin'])->group(function () {
+    Route::get('/admin/cartes', [CardController::class, 'index']);
+    Route::post('/admin/cartes/generate', [CardController::class, 'generate']);
+});
+
+// Routes protégées Agent (CheckIsAgent middleware)
+Route::middleware(['auth:sanctum', 'isAgent'])->group(function () {
+    Route::get('/agent/dashboard', [AgentController::class, 'dashboard']);
+}); */
+
+// Route pour récupérer l'utilisateur connecté (dans React pour lire le rôle)
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
