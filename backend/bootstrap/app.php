5766a5b49ef9b80d68a0ccefe686627d9d5efb1d
EthaneBokinga <bokingaethanenathan@gmail.com>
2026-05-12 03:34:23 +0100

    debug: ultra-light bootstrap to isolate startup crash

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Middleware\CheckIsAdmin;
-use App\Http\Middleware\CheckIsAgent;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,29 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-
-        // Trust Proxies (Railway)
-        $middleware->trustProxies(at: '*');
-        
-        // Enregistrement explicite du middleware CORS
-        $middleware->prependToGroup('api', HandleCors::class);
-        $middleware->prependToGroup('web', HandleCors::class);
-
         $middleware->validateCsrfTokens(except: [
             'api/*',
             'login',
             'logout',
             'sanctum/csrf-cookie',
-        ]);
-
-        $middleware->api(prepend: [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
-
-        $middleware->alias([
-            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
-            'isAdmin'  => CheckIsAdmin::class,
-            'isAgent'  => CheckIsAgent::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
