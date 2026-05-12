0898f6e2dd122d9803e987e37d79fc41279951fd
EthaneBokinga <bokingaethanenathan@gmail.com>
2026-05-12 14:29:21 +0100

    fix: forçage des cors sur index//

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return response()->json(['status' => 'ok', 'timestamp' => now()], 200);
+});
+
+Route::get('/up', function () {
+    return response()->json(['status' => 'ok', 'timestamp' => now()], 200);
 });
 
 require __DIR__.'/auth.php';
